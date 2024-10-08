88be5b3dbf9fd35cdf1072da69dc49f04fc54b58
Milton Pereira <miltonrpg@gmail.com>
2024-10-08 19:39:08 +0200

    Configuramos ALB, Listener y Targer Group,

--- maint.tf
+++ maint.tf
@@ -46,5 +46,53 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+# Crear un Application Load Balancer (ALB)
+resource "aws_lb" "nginx_alb" {
+  name               = "nginx-alb-milton"
+  internal           = false  # El ALB será público (accesible desde Internet)
+  load_balancer_type = "application"
+  security_groups    = [module.vpc.nginx_security_group_id]  # Usamos el grupo de seguridad del módulo VPC
+  subnets            = [module.vpc.public_subnet_id]  # El ALB debe estar en la subnet pública
+
+  enable_deletion_protection = false  # No proteger el ALB de eliminaciones
+  tags = {
+    Name = "nginx-alb"
+  }
+}
+
+
+# Listener para el ALB en el puerto 80
+resource "aws_lb_listener" "nginx_listener" {
+  load_balancer_arn = aws_lb.nginx_alb.arn  # Conectar el listener al ALB
+  port              = 80                    # Escuchar en el puerto 80 (HTTP)
+  protocol          = "HTTP"                # Protocolo HTTP
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.nginx_target_group.arn  # Redirigir el tráfico al target group
+  }
+}
+
+# Target Group para ECS
+resource "aws_lb_target_group" "nginx_target_group" {
+  name     = "nginx-target-group-milton"
+  port     = 80                      # Recibe tráfico en el puerto 80
+  protocol = "HTTP"                  # Protocolo HTTP
+  vpc_id   = module.vpc.vpc_id       # Asociamos el Target Group con la VPC
+
+  health_check {
+    path     = "/"                   # Verificación de salud en la raíz "/"
+    protocol = "HTTP"
+    port     = "80"
+  }
+}
+
+# Asociar la tarea de ECS con el Target Group
+resource "aws_lb_target_group_attachment" "nginx_target_attachment" {
+  target_group_arn = aws_lb_target_group.nginx_target_group.arn  # Usar el ARN del Target Group que creamos
+  target_id        = aws_ecs_service.nginx_service.id            # Asociar con el servicio ECS
+  port             = 80                                          # Enrutar el tráfico al puerto 80 de las tareas
+}
+
 
 
